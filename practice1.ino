67a5343e98b7f65787b67505c215ee0d1d2542b6
pablocontreras <pablo_contreras_1997@outlook.com>
2020-02-19 13:30:22 -0600

    first commit

---
+++ practice1.ino
@@ -0,0 +1,46 @@
+int counterDecenas = 0;
+int counterUnidades = 0; 
+const int boton = 53; 
+const int numbers[] = {63, 6, 91, 79, 102, 109, 125, 7, 127, 103}; //0,1,2,3,4,5,6,7,8,9
+int entradaBoton = 0; 
+int i = 0;
+void setup() {
+  // put your setup code here, to run once:
+  DDRC = OUTPUT;
+  DDRA = OUTPUT;
+  pinMode(boton, INPUT);
+
+  DDRA = 0;
+  DDRC = 0;
+}
+
+void loop() {
+  entradaBoton = digitalRead(boton);
+  
+  if( entradaBoton == LOW && counterUnidades < 9){
+    delay(200);
+    counterUnidades = counterUnidades + 1;
+    DDRC = numbers[counterUnidades];
+  }else if(entradaBoton == LOW && counterUnidades > 8 ){
+  
+    counterUnidades = 0;
+    counterDecenas = counterDecenas + 1;
+    DDRC = numbers[counterUnidades];
+    DDRA = numbers[counterDecenas];
+  }
+
+  
+  
+}
+
+/*
+ * Indice
+ * Objetivo
+ * Intruduccion
+ * Referencia
+ * Figuras citadas
+ * Problema
+ * Material
+ * SW
+ * Anexo de instalacion y conexión de arduino
+ */
